740fd0b248e85dd24a1c9c9b158a452600e72ae5
DmytroVoitovych <begunec23@gmail.com>
2022-06-26 15:34:43 +0200

    ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const colorRandom = document.querySelector('[data-start]');
+const colorRandomStop = document.querySelector('[data-stop]');
+const backgroundColorBody = document.querySelector('body');
+colorRandomStop.setAttribute('disabled', 'disabled');
 
+let interval = null; // переменная id интервала 
+const bodyChangeColor = () => {
+    colorRandom.setAttribute("disabled", "disabled");
+    colorRandomStop.removeAttribute('disabled');
+
+    if (colorRandom.hasAttribute('disabled')); {
+    colorRandomStop.removeAttribute('disabled'); 
+}
+    const startInterval = setInterval(() => {
+      backgroundColorBody.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  
+    return interval = startInterval;
+};
+
+colorRandomStop.addEventListener('click', () => {
+   colorRandomStop.setAttribute('disabled', 'disabled'); 
+   colorRandom.removeAttribute('disabled');
+   return  clearInterval(interval);
+       
+});
+    
+colorRandom.addEventListener('click', bodyChangeColor);    
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,62 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const start = document.querySelector('[type="submit"]');
+const step = document.querySelector('[name="step"]');
+const delayQ = document.querySelector('[name="delay"]');
+const amount = document.querySelector('[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+    
+  return  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve(
+          {
+            position,
+            delay
+          }
+        )
+      } else {
+        // Reject
+        reject({
+          position,
+          delay
+        }
+        )
+      }
+    }, delay)
+  });
+ 
+};
+//функция контроля кнопки по времени
+const num = 2600;
+const disabledButton = () => {
+  if (delayQ.value != "" && step.value != "")
+  { return +delayQ.value + (+step.value * +amount.value + +num); }
+  else {
+   return +amount.value + +num + 400;
   }
-}
+};
+
+//генерация по клику
+start.addEventListener('click', () => {
+  start.setAttribute('disabled', 'disabled');
+  setTimeout(() => start.removeAttribute('disabled'),
+   disabledButton());
+  for (let i = 0; i < amount.value; i += 1) {
+    setTimeout(() => {
+      createPromise(step.value, delayQ.value).then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+           Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }).catch(({ position, delay }) => {
+         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);;
+      });
+    }, i * step.value);
+  };
+  });
+
+const stopLink = (e) => e.preventDefault();
+start.addEventListener('click', stopLink);
